13d12418ff0f6ff02453cda9db0b6f8a6a29e76b
Jugoslav Jeftenic <jugoslav.jeftenic.su@gmail.com>
2023-04-26 18:37:45 +0200

    WDS - React Tutorial

--- src/App.js
+++ src/App.js
@@ -1,11 +1,35 @@
 import React from 'react';
+import { Link } from "react-router-dom";
 
 function App() {
 	return (
 		<>
 			<h1>React Tutorial</h1>
-			<p>Web Dev Simplified</p>
-			<a href='https://www.youtube.com/playlist?list=PLZlA0Gpn_vH_NT5zPVp18nGe_W9LqBDQK'>Tutorial playlist</a>
+			<p>
+				<span>Web Dev Simplified </span>
+				<a
+					href='https://www.youtube.com/playlist?list=PLZlA0Gpn_vH_NT5zPVp18nGe_W9LqBDQK'
+					target='blank'>
+					tutorial playlist
+				</a>
+				<span>.</span>
+			</p>
+			<div>
+				<h2>npm biblioteke</h2>
+				<ul>
+					<li>npm i</li>
+					<li>npm i react-router-dom</li>
+					<li>npm i axios</li>
+				</ul>
+			</div>
+			<hr />
+			<div>
+				<h2>aplikacije</h2>
+				<ol>
+					<li><Link to="pokemon">Go to Pokemon</Link></li>
+				</ol>
+			</div>
+
 		</>
 	);
 }
--- src/index.js
+++ src/index.js
@@ -1,10 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import App from './App';
+
+import Pokemon from './01-pokemon/Pokemon';
+
+const router = createBrowserRouter([
+	{
+		path: "/",
+		element: <App />,
+	},
+	{
+		path: "/pokemon",
+		element: <Pokemon />,
+	}
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+	<React.StrictMode>
+		<RouterProvider router={router} />
+		{/* <App /> */}
+	</React.StrictMode>
 );
