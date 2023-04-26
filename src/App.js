be064c50d70ef256ab5e3f9b26347ad4d9434a58
Jugoslav Jeftenic <jugoslav.jeftenic.su@gmail.com>
2023-04-26 17:44:54 +0200

    WDS - React Tutorial

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+	return (
+		<>
+			<h1>React Tutorial</h1>
+			<p>Web Dev Simplified</p>
+			<a href='https://www.youtube.com/playlist?list=PLZlA0Gpn_vH_NT5zPVp18nGe_W9LqBDQK'>Tutorial playlist</a>
+		</>
+	);
+}
+
+export default App;
